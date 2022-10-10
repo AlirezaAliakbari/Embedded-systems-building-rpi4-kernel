e38b167369b9e6f8e46015d98e987b400c4a0474
Alireza Aliakbari <115478245+AlirezaAliakbari@users.noreply.github.com>
2022-10-10 17:26:58 +0330

    Add files via upload

---
+++ elinux2/Makefile/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mult.h"
+
+int main()
+{
+ printf("10 * 12 = %d", mult(10, 12));
+ return 0;
+}
---
+++ elinux2/Makefile/mult.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "mult.h"
+int mult(int a, int b)
+{
+ return a * b;
+}
